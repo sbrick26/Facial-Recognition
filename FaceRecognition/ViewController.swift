b09ac7dde35d3a6de668df5b843ac689223e0896
sbrick26 <swayambarik@gmail.com>
2021-11-01 15:36:02 -0700

    dispatch queue added to for main thread

--- FaceRecognition/ViewController.swift
+++ FaceRecognition/ViewController.swift
@@ -6,14 +6,39 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func signInButtonClicked(_ sender: Any) {
+        
+        let authContext = LAContext()
+        var error: NSError?
+        
+        
+        if authContext.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            authContext.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: "Use FaceID To Log In") { (isSuccess, error) in
+                if isSuccess {
+                    
+                    DispatchQueue.main.async {
+                        self.performSegue(withIdentifier: "toSecondViewController", sender: nil)
+                    }
+                }
+                else {
+                    DispatchQueue.main.async {
+                        self.myLabel.text = "Error!"
+                    }
+                    
+                }
+            }
+        }
+    }
+    
 }
 
